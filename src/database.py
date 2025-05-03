575601bec87a394635eb2145b9024efccf7ecc68
Xsu <xsusha03@gmail.com>
2025-05-03 13:19:46 +0300

    g

--- src/database.py
+++ src/database.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-engine = create_async_engine("DATABASE_URL",'postgresql+psycopg://kubsu:kubsu@localhost:5432/kubsu', echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql+psycopg://kubsu:kubsu@localhost:5432/kubsu')
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 

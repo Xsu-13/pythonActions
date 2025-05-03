cda9029d2236cb94ec13471c166efccdfe220d09
Xsu <xsusha03@gmail.com>
2025-05-03 13:21:42 +0300

    g

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 
 DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql+psycopg://kubsu:kubsu@localhost:5432/kubsu')
